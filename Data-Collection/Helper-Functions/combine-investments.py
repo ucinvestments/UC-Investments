0c7b014ac964c92200bac0920cf751bdb667cee9
Alex <60298521+g4cko@users.noreply.github.com>
2024-04-30 04:34:56 -0700

    Added fund types and formalized naming scheme on files

--- Data-Collection/Helper-Functions/combine-investments.py
+++ Data-Collection/Helper-Functions/combine-investments.py
@@ -12,16 +12,20 @@ def combine_investments(dir_path, output_file):
 
             df = pd.read_csv(file_path)
 
-            #
+            investment_type = " ".join(source.split('-')[1:]).replace("UCRP", "").replace("GEP","").replace(".csv","")
+
             df['Source'] = source
+            df['Investment_Type'] = investment_type
 
             combined_df = pd.concat([combined_df, df], ignore_index=True)
 
-    result_df = combined_df.groupby('Name').agg(
+    result_df = combined_df.groupby(['Name', 'Investment_Type']).agg(
+      
         Total_investment=pd.NamedAgg(column='Amount', aggfunc='sum'),
         UCRP_Amount=pd.NamedAgg(column='Amount', aggfunc=lambda x: sum(x[combined_df['Source'].str.startswith('UCRP')])),
         GEP_Amount=pd.NamedAgg(column='Amount', aggfunc=lambda x: sum(x[combined_df['Source'].str.startswith('GEP')])),
     ).reset_index()
+    result_df = result_df[['Name','Investment_Type', 'Total_investment', 'UCRP_Amount', 'GEP_Amount']]
 
     
     result_df.to_csv(output_file, index=False)
