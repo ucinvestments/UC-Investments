bf11e41006336734288481295394e8d31e3a58de
The Arctesian <87273292+TheArctesian@users.noreply.github.com>
2024-05-02 10:25:09 -0700

    pooing soo much rn

--- Webapp/src/lib/pi.svelte
+++ Webapp/src/lib/pi.svelte
@@ -4,10 +4,10 @@
 
   export let filteredData;
   export let selectedSlice;
+  export let activeButton;
   let width = 350;
   let height = 400;
   let radius = Math.min(width, height) / 2;
-
   let svg;
   let g;
   let color;
@@ -15,6 +15,10 @@
   let arcPath;
   let arcLabel;
   const dispatch = createEventDispatcher();
+
+  function getRandomColor() {
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+  }
 
   function cap(string) {
     return string
@@ -24,63 +28,124 @@
   }
 
   function drawChart() {
-  if (!g) return;
+    if (!g) return;
 
-  const validData = filteredData.length > 0 ? filteredData : [];
+    const validData = filteredData.length > 0 ? filteredData : [];
 
-  const update = g.selectAll(".arc").data(pie(validData));
+    const update = g.selectAll(".arc").data(pie(validData));
 
-  const exit = update.exit().transition().duration(500).remove();
+    const exit = update.exit().transition().duration(500).remove();
 
-  exit.select("path").attrTween("d", function (d) {
-    const interpolate = d3.interpolate(d, { startAngle: d.endAngle, endAngle: d.endAngle });
-    return function (t) {
-      return arcPath(interpolate(t));
-    };
-  });
-
-  exit.select("text").style("opacity", 0);
-
-  const enter = update.enter().append("g").attr("class", "arc");
-  const arcs = enter.merge(update);
-
-  enter.append("path").on("click", handleSliceClick);
-
-  arcs
-    .select("path")
-    .transition()
-    .duration(500)
-    .attrTween("d", function (d) {
-      const interpolate = d3.interpolate(this._current, d);
-      this._current = interpolate(0);
+    exit.select("path").attrTween("d", function (d) {
+      const interpolate = d3.interpolate(d, {
+        startAngle: d.endAngle,
+        endAngle: d.endAngle,
+      });
       return function (t) {
         return arcPath(interpolate(t));
       };
-    })
-    .attr("fill", (d) => color(d.data.key))
-    .attr("stroke", (d) => (d.data === selectedSlice ? "white" : "none"))
-    .attr("stroke-width", (d) => (d.data === selectedSlice ? "2px" : "0px"));
+    });
 
-  arcs
-    .select("text")
-    .transition()
-    .duration(100)
-    .attr("transform", (d) => `translate(${arcLabel.centroid(d)})`)
-    .text((d) => (d.data === selectedSlice ? cap(d.data.key) : ""))
-    .style("opacity", 1);
+    exit.select("text").style("opacity", 0);
 
-  enter
-    .append("text")
-    .style("font-size", "12px")
-    .style("fill", "var(--black)")
-    .style("text-anchor", "middle")
-    .style("opacity", 0)
-    .transition()
-    .duration(100)
-    .style("opacity", 1);
+    const enter = update.enter().append("g").attr("class", "arc");
+    const arcs = enter.merge(update);
 
-  update.raise();
-}
+    enter.append("path").on("click", handleSliceClick);
+
+    if (activeButton == "Company") {
+      arcs
+        .select("path")
+        .transition()
+        .duration(500)
+        .attrTween("d", function (d) {
+          const interpolate = d3.interpolate(this._current, d);
+          this._current = interpolate(0);
+          return function (t) {
+            return arcPath(interpolate(t));
+          };
+        })
+        .attr("fill", (d) => getRandomColor())
+        .attr("stroke", (d) => (d.data === selectedSlice ? "white" : "none"))
+        .attr("stroke-width", (d) =>
+          d.data === selectedSlice ? "2px" : "0px",
+        );
+
+      arcs
+        .select("text")
+        .transition()
+        .duration(100)
+        .attr("transform", (d) => `translate(${arcLabel.centroid(d)})`)
+        .text((d) => (d.data === selectedSlice ? cap(d.data["asset"]) : ""))
+        .style("opacity", 1);
+    } else if (activeButton == "Fund") {
+      arcs
+        .select("path")
+        .transition()
+        .duration(500)
+        .attrTween("d", function (d) {
+          const interpolate = d3.interpolate(this._current, d);
+          this._current = interpolate(0);
+          return function (t) {
+            return arcPath(interpolate(t));
+          };
+        })
+        .attr("fill", (d) => getRandomColor())
+        .attr("stroke", (d) => (d.data === selectedSlice ? "white" : "none"))
+        .attr("stroke-width", (d) =>
+          d.data === selectedSlice ? "2px" : "0px",
+        );
+
+      arcs
+        .select("text")
+        .transition()
+        .duration(100)
+        .attr("transform", (d) => `translate(${arcLabel.centroid(d)})`)
+        .text((d) =>
+          d.data === selectedSlice ? cap(d.data["Asset Name"]) : "",
+        )
+        .style("opacity", 1);
+    } else {
+      arcs
+        .select("path")
+        .transition()
+        .duration(500)
+        .attrTween("d", function (d) {
+          const interpolate = d3.interpolate(this._current, d);
+          this._current = interpolate(0);
+          return function (t) {
+            return arcPath(interpolate(t));
+          };
+        })
+        .attr("fill", (d) => getRandomColor())
+        .attr("stroke", (d) => (d.data === selectedSlice ? "white" : "none"))
+        .attr("stroke-width", (d) =>
+          d.data === selectedSlice ? "2px" : "0px",
+        );
+
+      arcs
+        .select("text")
+        .transition()
+        .duration(100)
+        .attr("transform", (d) => `translate(${arcLabel.centroid(d)})`)
+        .text((d) =>
+          d.data === selectedSlice ? cap(d.data["Asset Name"]) : "",
+        )
+        .style("opacity", 1);
+      pie = d3.pie().value((d) => d["total_investment"]);
+    }
+    enter
+      .append("text")
+      .style("font-size", "12px")
+      .style("fill", "var(--black)")
+      .style("text-anchor", "middle")
+      .style("opacity", 0)
+      .transition()
+      .duration(100)
+      .style("opacity", 1);
+
+    update.raise();
+  }
 
   onMount(() => {
     color = d3.scaleOrdinal([
@@ -100,8 +165,13 @@
       "var(--pacific)",
       "var(--stone-pine)",
     ]);
-
-    pie = d3.pie().value((d) => d.value);
+    if (activeButton == "Company") {
+      pie = d3.pie().value((d) => d["total investment"]);
+    } else if (activeButton == "Fund") {
+      pie = d3.pie().value((d) => d["Total Investment"]);
+    } else {
+      pie = d3.pie().value((d) => d["Total Iℂnvest∈d"]);
+    }
     arcPath = d3
       .arc()
       .outerRadius(radius - 10)
--- Webapp/src/routes/+page.svelte
+++ Webapp/src/routes/+page.svelte
@@ -4,14 +4,42 @@
   import Icon from "@iconify/svelte";
   import Pi from "$lib/pi.svelte";
   import { onMount } from "svelte";
-
+  let end;
   type button = "Asset" | "Company" | "Fund";
 
-  const API = "https://uc-investments-80f94956a47a.herokuapp.com/"
+  function asset() {
+    activeButton = "Asset";
+    fetchListedAssets();
+  }
+  function company() {
+    activeButton = "Company";
+    fetchListedAssets();
+  }
+  function fund() {
+    activeButton = "Fund";
+    fetchListedAssets();
+  }
+
+  function estim() {
+    if (estimation == "true") {
+      estimation = "false";
+      fetchListedAssets();
+    } else {
+      estimation = "true";
+      fetchListedAssets();
+    }
+  }
+
+  const API = "https://uc-investments-80f94956a47a.herokuapp.com";
   let activeButton: button = "Company"; // Initial value
   let searchTerm = ""; // Initialize a variable to store the search term
   let totalInvestedInChart = 0;
-
+  let data;
+  let selectedSlice; // Initialize a variable to store the selected slice
+  let filteredData;
+  let loading = true;
+  let composition = "true"; // combine share types or not
+  let estimation = "true"; // are we using estimates or not
   function cap(s) {
     if (s && typeof s === "string") {
       return s
@@ -23,52 +51,49 @@
     }
   }
 
+  function formatNumber(num) {
+    /**
+     * Formats a number as a human-readable string with commas.
+     *
+     * @param {number} num - The number to be formatted.
+     * @returns {string} The formatted number as a string.
+     */
+    // Convert the number to a string
+
+    let numStr = num.toString();
+    let [integerPart, decimalPart] = numStr.split(".");
+    let formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return formattedInteger;
+  }
+
   async function fetchListedAssets() {
+    loading = true;
+    if (activeButton == "Company") {
+      end = "/company-composition/" + composition + "/" + estimation;
+    } else if (activeButton == "Fund") {
+      end = "/listed-assets";
+    } else {
+      end = "/asset-classes";
+    }
+
     try {
-      const response = await fetch(API + "listed-assets");
+      const response = await fetch(API + end);
       if (!response.ok) {
         throw new Error("Failed to fetch listed assets");
       }
       const d = await response.json();
-      data = d.data;
+      data = d;
+      selectedSlice = data[0]; // Initialize a variable to store the selected slice
+      filteredData = data;
+      loading = false;
       console.log(data);
+
+      sumTotalInvestments();
     } catch (err) {
       console.log(err.message);
     }
   }
 
-  let data = [
-    { key: "microsfot", value: 10, type: "bad", fundingSource: [1, 2, 3] },
-    { key: "big bad", value: 20, type: "bad", fundingSource: [1, 2, 3] },
-    { key: "killing people", value: 30, type: "bad", fundingSource: [1, 2, 3] },
-    { key: "eating people", value: 40, type: "bad", fundingSource: [1, 2, 3] },
-    {
-      key: "destroying housing market",
-      value: 40,
-      type: "bad",
-      fundingSource: [1, 2, 3],
-    },
-    {
-      key: "in socites of wealth",
-      value: 40,
-      type: "bad",
-      fundingSource: [1, 2, 3],
-    },
-    { key: "guy dobirid", value: 40, type: "bad", fundingSource: [1, 2, 3] },
-    {
-      key: "composition book",
-      value: 40,
-      type: "bad",
-      fundingSource: [1, 2, 3],
-    },
-    { key: "cocaine", value: 40, type: "bad", fundingSource: [1, 2, 3] },
-    { key: "nicotine", value: 40, type: "bad", fundingSource: [1, 2, 3] },
-    { key: "chadonay", value: 40, type: "bad", fundingSource: [1, 2, 3] },
-  ];
-
-  let selectedSlice = data[0]; // Initialize a variable to store the selected slice
-
-  let filteredData = data;
   // Handler function for the 'sliceClicked' event
   function handleSliceClicked(event) {
     searchTerm = "";
@@ -77,28 +102,45 @@
   }
   function filterSearch() {
     const normalizedSearchTerm = searchTerm.trim().toLowerCase();
-    filteredData = data.filter(
-      (item) =>
-        item.key.toLowerCase().includes(normalizedSearchTerm) ||
-        item.type.toLowerCase().includes(normalizedSearchTerm) ||
-        item.fundingSource.some((fund) =>
-          fund.toString().includes(normalizedSearchTerm),
-        ),
-    );
+    if (activeButton == "Company") {
+      filteredData = data.filter((item) =>
+        item["asset"].toLowerCase().includes(normalizedSearchTerm),
+      );
+    } else if (activeButton == "Asset") {
+      filteredData = data.filter((item) =>
+        item["Asset Type"].toLowerCase().includes(normalizedSearchTerm),
+      );
+    } else {
+      filteredData = data.filter((item) =>
+        item["Asset Name"].toLowerCase().includes(normalizedSearchTerm),
+      );
+    }
     console.log(filteredData);
   }
 
   function handleSearch() {
     const normalizedSearchTerm = searchTerm.trim().toLowerCase();
-    const matchingSlice = data.find((item) =>
-      item.key.toLowerCase().includes(normalizedSearchTerm),
-    );
+    let matchingSlice;
+    if (activeButton == "Company") {
+      matchingSlice = data.find((item) =>
+        item["asset"].toLowerCase().includes(normalizedSearchTerm),
+      );
+    } else if (activeButton == "Asset") {
+      matchingSlice = data.find((item) =>
+        item["Asset Type"].toLowerCase().includes(normalizedSearchTerm),
+      );
+    } else {
+      matchingSlice = data.find((item) =>
+        item["Asset Name"].toLowerCase().includes(normalizedSearchTerm),
+      );
+    }
     if (matchingSlice) {
       selectedSlice = matchingSlice; // Assign the entire object
       console.log("Search found:", selectedSlice);
       filterSearch();
+      sumTotalInvestments();
     } else {
-      selectedSlice = selectedSlice;
+      selectedSlice = data[0];
       console.log("No matching slice found");
     }
   }
@@ -106,9 +148,15 @@
   function sumTotalInvestments() {
     let totalSum = 0;
     for (let i = 0; i < filteredData.length; i++) {
-      totalSum += parseInt(filteredData[i]["Total Investment"]);
+      if (activeButton == "Company") {
+        totalSum += parseInt(filteredData[i]["total investment"]);
+      } else if (activeButton == "Fund") {
+        totalSum += parseInt(filteredData[i]["Total Investment"]);
+      } else {
+        totalSum += parseInt(filteredData[i]["Total Iℂnvest∈d"]);
+      }
     }
-    return totalSum;
+    totalInvestedInChart = totalSum;
   }
 
   onMount(() => {
@@ -129,63 +177,104 @@
   investments. Explore where this money goes below:
 </p>
 <div class="flex justify-between but text-lg m-1">
-  <button
-    class:active-button={activeButton === "Company"}
-    on:click={() => (activeButton = "Company")}>By Company</button
+  <button class:active-button={activeButton === "Company"} on:click={company}
+    >By Company</button
   >
 
-  <button
-    class:active-button={activeButton === "Asset"}
-    on:click={() => (activeButton = "Asset")}>By Asset Class</button
+  <button class:active-button={activeButton === "Asset"} on:click={asset}
+    >By Asset Class</button
   >
 
-<button
-    class:active-button={activeButton === "Fund"}
-    on:click={() => (activeButton = "Fund")}>By Fund</button
+  <button class:active-button={activeButton === "Fund"} on:click={fund}
+    >By Fund</button
   >
 </div>
 
-<div class="cont">
-  <div class="image-container">
-    <div class="search-container text-center">
-      <input
-        type="text"
-        placeholder="Search..."
-        bind:value={searchTerm}
-        on:input={handleSearch}
-        class="p-3 rounded-md w-5/6 bg-white focus:outline-none focus:ring-2 focus:ring-[var(--founder)]"
+{#if loading}
+  <p class="text-center h-full m-auto justify-center">loading</p>
+{:else}
+  <div class="cont">
+    <div class="image-container">
+      <div class="search-container text-center">
+        <input
+          type="text"
+          placeholder="Search..."
+          bind:value={searchTerm}
+          on:input={handleSearch}
+          class="p-3 rounded-md w-5/6 bg-white focus:outline-none focus:ring-2 focus:ring-[var(--founder)]"
+        />
+      </div>
+      <Pi
+        {filteredData}
+        {selectedSlice}
+        {activeButton}
+        on:sliceClicked={handleSliceClicked}
       />
-    </div>
-    <Pi {filteredData} {selectedSlice} on:sliceClicked={handleSliceClicked} />
-    <p class="text-center">Total value of chart ${totalInvestedInChart}</p>
-  </div>
-
-
-  <div class="text-container">
-    <div class="text-left x p-3 rounded-sm">
-      {#if activeButton == "Asset"}
       <p class="text-center">
-        <b>Asset Name: </b>{cap(selectedSlice.key)}
-      </p>
-      <br />
-
-      {:else if activeButton == "Company"}
-      <p class="text-center">
-        <b>Company Name: </b>{cap(selectedSlice.key)}
-      </p>
-      <p class=""><b>Total Invested: </b>{selectedSlice.value}</p>
-        <p class=""><b>Company Type: </b>{selectedSlice.type}</p>
-      {:else}{/if}
-      <p class="">
-        <br />
-        <b>Funding Source: </b>
-        {#each selectedSlice.fundingSource as fund}
-          <p class=" rounded-sm p-1">{fund}</p>
-        {/each}
+        Total value of chart ${formatNumber(totalInvestedInChart)}
       </p>
     </div>
+
+    <div class="text-container">
+      <div class="text-left x p-3 rounded-sm">
+        {#if activeButton == "Fund"}
+          <p class="text-center">
+            <b>{cap(selectedSlice["Asset Name"])}</b>
+          </p>
+          <br />
+
+          <p><b>Asset Type: </b>{selectedSlice["Asset Type"]}</p>
+          <br />
+          <br />
+          <p>
+            <b>Funding Sources: </b>
+
+            {#each selectedSlice["Funding Sources"] as f}
+              <p class=" rounded-sm px-2">
+                <b>- {f["Source:"]} : </b>${formatNumber(f["Ammount"])}
+              </p>
+            {/each}
+          </p>
+        {:else if activeButton == "Company"}
+          <p class="text-center text-2xl">
+            <b>{cap(selectedSlice["asset"])}</b>
+          </p>
+          <p class="text-center">
+            <b>Total Invested: </b>${formatNumber(
+              selectedSlice["total investment"],
+            )}
+          </p>
+          <br />
+          <b>Funding Sources: </b>
+          {#each selectedSlice["funding sources"] as fund}
+            <p class=" rounded-sm px-2">
+              <b>- {fund["fund name:"]} : </b>${formatNumber(
+                fund["ammount_invested"],
+              )}
+            </p>
+          {/each}
+
+          <!-- <p class=""><b>Total Invested: </b>{selectedSlice.value}</p> -->
+          <!-- <p class=""><b>Company Type: </b>{selectedSlice.type}</p> -->
+        {:else}
+          <p class="text-center">
+            <b>Asset Name: </b>{cap(selectedSlice["Asset Name"])}
+          </p>
+          <br />
+          <b>Asset Type: </b>{selectedSlice["Asset Type"]}
+          <br />
+          <p>
+            <b>Funding Source: </b>
+            {#each selectedSlice["Funding Sources"] as fund}
+              <p class=" rounded-sm p-1">{fund}</p>
+            {/each}
+          </p>
+        {/if}
+      </div>
+    </div>
   </div>
-</div>
+{/if}
+<p class="p-3">Note: The UC does not have direct investments in any of these companies. All of this data is constructed by examining the composition of the index funds we are invested in, and accumulating the holdings across all funds. The composition of these funds changes day to day, so this data is as of each fund’s most recent disclosure. Additionally, most funds do not publish full holdings, but instead their top 10 holdings, which is why you will see a significant amount of the assets grayed out. We’ve developed novel methodology to make evidence-based predictions of the compositions of our largest funds, and you can view our predictions by clicking “Show informed estimate”</p>
 
 <style lang="postcss">
   .active-button {
