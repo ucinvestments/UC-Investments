b394b72e7925522f7a493e957db7842a9dedf4b0
theArctesian <stephen.d.okita@gmail.com>
2025-08-28 21:48:28 -0700

    anal fixes?

--- Webapp/svelte.config.js
+++ Webapp/svelte.config.js
@@ -3,7 +3,20 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    csp: {
+      mode: 'hash',
+      directives: {
+        'script-src': ['self', 'unsafe-eval', 'https://us-assets.i.posthog.com', 'https://vercel.live'],
+        'connect-src': ['self', 'https://us.i.posthog.com', 'https://us-assets.i.posthog.com', 'https://uc-investments-80f94956a47a.herokuapp.com', 'https://vitals.vercel-analytics.com'],
+        'img-src': ['self', 'data:', 'https:'],
+        'style-src': ['self', 'unsafe-inline', 'https://fonts.googleapis.com'],
+        'font-src': ['self', 'https://fonts.gstatic.com'],
+        'frame-src': ['self'],
+        'object-src': ['none'],
+        'base-uri': ['self']
+      }
+    }
   },
   preprocess: vitePreprocess()
 };
